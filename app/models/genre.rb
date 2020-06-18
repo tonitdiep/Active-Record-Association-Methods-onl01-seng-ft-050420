6afbd85502af34250168ec5217ff1268e8abc41e
Toni  Diep <adiep123@gmail.com>
2020-06-18 03:32:51 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -8,10 +8,14 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
+    self.artists.count
     # return the number of artists associated with the genre
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artists.collect do |t|
+      t.name
+    end
   end
 end
